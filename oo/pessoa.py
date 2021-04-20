79da6587cd60a715f07125ee59cba08b8e166a53
akiosonehara <akiosonehara@gmail.com>
2021-04-20 00:57:32 -0300

    Criado e removido atributo dinâmico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,24 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Old {id(self)}'
 
 
-if __name__ == '__main__':
-    p = Pessoa('Alexandre')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Akio Sonehara'
-    print(p.nome)
-    print(p.idade)
+if __name__ == '__main__':  #dunder main
+    akio = Pessoa(nome='Akio')
+    alexandre = Pessoa(akio, nome='Alexandre')
+    print(Pessoa.cumprimentar(alexandre))
+    print(id(alexandre))
+    print(alexandre.cumprimentar())
+    print(alexandre.nome)
+    print(alexandre.idade)
+    for filho in alexandre.filhos:
+        print(filho.nome)
+    alexandre.sobrenome = 'Sonehara'
+    del alexandre.filhos
+    print(alexandre.__dict__)   #dunder dict
+    print(akio.__dict__)
